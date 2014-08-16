12b4e9129bbaa357c2168e4897b73137e39618a8
Mateusz <mateusz@gepert.info>
2014-08-16 12:24:10 +0200

    Week3 update

--- week3/yahtzee/game_logic_yahtzee.py
+++ week3/yahtzee/game_logic_yahtzee.py
@@ -1,4 +1,3 @@
-__author__ = 'advena'
 """
 Planner for Yahtzee
 Simplifications:  only allow discard and roll, only score against upper level
@@ -8,13 +7,14 @@ Simplifications:  only allow discard and roll, only score against upper level
 # import codeskulptor
 # codeskulptor.set_timeout(20)
 
+
 def gen_all_sequences(outcomes, length):
     """
     Iterative function that enumerates the set of all sequences of
     outcomes of given length.
     """
 
-    answer_set = set([()])
+    answer_set = [()]
     for dummy_idx in range(length):
         temp_set = set()
         for partial_sequence in answer_set:
@@ -23,7 +23,10 @@ def gen_all_sequences(outcomes, length):
                 new_sequence.append(item)
                 temp_set.add(tuple(new_sequence))
         answer_set = temp_set
-    return answer_set
+    return sorted(answer_set)
+
+
+
 
 
 def score(hand):
@@ -35,7 +38,16 @@ def score(hand):
 
     Returns an integer score
     """
-    return 0
+    current_hand = {}
+    for dice in hand:
+        if not current_hand.get(dice):
+            current_hand[dice] = dice
+        else:
+            current_hand[dice] += dice
+
+    #compute the current score for each dice
+
+    return max(current_hand.values())
 
 
 def expected_value(held_dice, num_die_sides, num_free_dice):
@@ -49,7 +61,17 @@ def expected_value(held_dice, num_die_sides, num_free_dice):
 
     Returns a floating point expected value
     """
-    return 0.0
+
+    outcome = ()
+    for die in range(1, num_die_sides + 1):
+        outcome +=(die, )
+    possible_outcomes = gen_all_sequences(outcome, num_free_dice)
+    output = 0
+    for single_output in possible_outcomes:
+        current_score = score(single_output + held_dice)
+        output += current_score
+
+    return output/(len(possible_outcomes)*1.0)
 
 
 def gen_all_holds(hand):
@@ -60,7 +82,18 @@ def gen_all_holds(hand):
 
     Returns a set of tuples, where each tuple is dice to hold
     """
-    return set([()])
+
+    mask = sorted(gen_all_sequences((1,0), len(hand)))
+    answer_set = []
+    for current_mask in mask:
+        temp = []
+        for indx in range(len(current_mask)):
+            if current_mask[indx] == 1:
+                temp.append(hand[indx]);
+        answer_set.append(tuple(temp))
+    return set(answer_set)
+
+
 
 
 
@@ -75,24 +108,32 @@ def strategy(hand, num_die_sides):
     Returns a tuple where the first element is the expected score and
     the second element is a tuple of the dice to hold
     """
-    return (0.0, ())
+    all_holds = gen_all_holds(hand)
+    expected_values = {}
+    for hold in all_holds:
+        num_free_dice = len(hand) - len(hold)
+        current_expexted_value = expected_value(hold, num_die_sides, num_free_dice)
+        expected_values[current_expexted_value] = hold
+
+    max_value = max(expected_values.keys())
+    return tuple((max_value, expected_values[max_value]))
 
 
-def run_example():
-    """
-    Compute the dice to hold and expected score for an example hand
-    """
-    num_die_sides = 6
-    hand = (1, 1, 1, 5, 6)
-    hand_score, hold = strategy(hand, num_die_sides)
-    print "Best strategy for hand", hand, "is to hold", hold, "with expected score", hand_score
+# def run_example():
+#     """
+#     Compute the dice to hold and expected score for an example hand
+#     """
+#     # num_die_sides = 6
+#     # hand = (1, 1, 1, 5, 6)
+#     # hand_score, hold = strategy(hand, num_die_sides)
+#     # print "Best strategy for hand", hand, "is to hold", hold, "with expected score", hand_score
+#     print strategy((1,), 6)
+#     print expected_value((1, ), 6, 5)
+# run_example()
 
 
-run_example()
-
-
-#import poc_holds_testsuite
-#poc_holds_testsuite.run_suite(gen_all_holds)
+# import poc_holds_testsuite
+# poc_holds_testsuite.run_suite(gen_all_holds)
 
 
 
--- week3/yahtzee/test_yahatzee.py
+++ week3/yahtzee/test_yahatzee.py
@@ -1,8 +1,32 @@
 from unittest import TestCase
+from game_logic_yahtzee import *
 
 __author__ = 'advena'
 
 
 class TestScore(TestCase):
-    def test_score(self):
-        self.fail()
+    def test_score_with_six_as_highest_value(self):
+        #given
+        output = score((1, 1, 1, 3, 6))
+        self.assertEqual(6, output)
+
+
+    def test_score_(self):
+        #given
+        output = score((6, 6, 5, 5, 5))
+        self.assertEqual(15, output)
+
+    def test_score_with_four_twos_one_six(self):
+        #given
+        output = score((2, 2, 2, 2, 6))
+        self.assertEqual(8, output)
+
+
+class TestExpected_value(TestCase):
+
+
+    def test_expected_value(self):
+        #given
+        output = expected_value((2, 1), 6, 3)
+        self.assertEqual(output, 6.472222222222222)
+
